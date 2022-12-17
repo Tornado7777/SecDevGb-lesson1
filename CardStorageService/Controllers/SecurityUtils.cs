1d1e9dbe997af470cd327b8ec0d7e87b92f30ed3
Tornado7777 <Tornadod@mail.ru>
2022-12-17 21:55:58 +0300

    hw1 1

---
+++ CardStorageService/Controllers/SecurityUtils.cs
@@ -0,0 +1,24 @@
+﻿using CardStorageService.Data;
+
+namespace CardStorageService.Controllers
+{
+    public static class SecurityUtils
+    {
+        public static bool CheckString(string value)
+        {
+            Char[] closedChar = new Char[] { '\\', '/', '\"', ' ', '\'' };
+            
+            if (string.IsNullOrEmpty(value)) 
+                return false;
+
+            string[] a = value.Split(closedChar);
+            
+            if (a.Length > 1)
+                return false;
+            
+            return true;
+        }
+
+       
+    }
+}
